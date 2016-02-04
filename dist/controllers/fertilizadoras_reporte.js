52131bee4e6fcced5080db62533c687a956208f2
ponyesteves <ajesteves@gmail.com>
2016-02-04 17:02:56 -0300

    clean dependencies for remote

--- dist/controllers/fertilizadoras_reporte.js
+++
@@ -1,9 +0,0 @@
-angular.module('Lm').controller('FertilizadorasReporteCtrl', function($rootScope, $scope, $http) {
-  var ruta;
-  ruta = $rootScope.rootPath + 'reporteador';
-  return $http.get(ruta, {
-    tipo: 'fertilizadoras'
-  }).then(function(resp) {
-    return $scope.obj = resp.data;
-  });
-});
--- dist/main.js
+++
@@ -1,3 +0,0 @@
-angular.module('Lm', ['ngRoute']).run(function($rootScope) {
-  return $rootScope.rootPath = 'http://localhost:3000/';
-});
